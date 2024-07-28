c8e7d0f4fc8cf17ac26a5abed04ed020592a8970
Yasser Bal <yasserbal@icloud.com>
2024-07-28 20:22:33 +0200

    enabled nerdfont - Not sure what effect this has

--- init.lua
+++ init.lua
@@ -20,14 +20,14 @@
 
 --]]
 
--- Set <space> as the leader key
--- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
+-- WARN - Set Leader first! | `:help mapleader`
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+-- If nerd font is installed - Not sure what this does yet
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
